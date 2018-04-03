dbcbc1265310b70213132afc4c878f5ba70ec15b
Paweł Dybiec <dybiec@windowslive.com>
2018-04-04 01:57:01 +0200

    Attractor

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include<vector>
 #include<unordered_set>
 #include<unordered_map>
+#include<algorithm>
 
 std::unordered_map<int,int> nn;//new numbers
-std::vector<std::vector<int> > G,G_rev;
+std::vector<std::vector<int> > G_or,G_rev;
 std::vector<bool> player;
+using subgraph=std::unordered_set<int>;
 std::vector<unsigned char>colours;
-int n0,n1,m,V;
+int n,n0,n1,m,V;
 void read()
 {
   scanf("%d%d%d",&n0,&n1,&m);
-  G.resize(n0+n1);
-  G_rev.resize(n0+n1);
-  colours.resize(n0+n1);
+  n=n0+n1;
+  G_or.resize(n);
+  G_rev.resize(n);
+  colours.resize(n);
   int cnt=0;
   for(int i=0;i<n0;i++)
   {
@@ -37,9 +40,33 @@ void read()
   {
     int a,b;
     scanf("%d%d",&a,&b);
-    G[nn[a]].push_back(nn[b]);
+    G_or[nn[a]].push_back(nn[b]);
     G_rev[nn[b]].push_back(nn[a]);
   }
+}
+subgraph attr(const subgraph &G, const subgraph &U, int cur_player)
+{
+  bool cont=true;
+  subgraph res=U;
+  while(cont)
+  {
+    cont=false;
+    for(int e:G)
+    {
+      if(res.count(e))continue;
+      if(player[e]==cur_player and std::any_of(G_or[e].begin(),G_or[e].end(),[U](int x){return U.count(x);}))
+      {
+        res.insert(e);
+        cont=true;
+      }
+      else if(player[e]!=cur_player and std::all_of(G_or[e].begin(),G_or[e].end(),[U](int x){return U.count(x);}))
+      {
+        res.insert(e);
+        cont=true;
+      }
+    }
+  }
+  return res;
 }
 int main()
 {
