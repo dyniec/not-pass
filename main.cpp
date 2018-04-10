b0246e422a450bf49afcda8556fb557736d6da99
Paweł Dybiec <pawel.to.malpa@gmail.com>
2018-04-10 23:58:22 +0200

    Keep original numbers of vertices

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include<utility>
 #include<tuple>
 
-std::unordered_map<int,int> nn;//new numbers
 std::vector<std::vector<int> > G_or,G_rev;
 std::vector<bool> player;
 using subgraph=std::unordered_set<int>;
@@ -44,36 +43,29 @@ void read()
   G_rev.resize(n);
   colours.resize(n);
   player.resize(n);
-  int cnt=0;
   for(int i=0;i<n0;i++)
   {
     int v,c;
     scanf("%d%d",&v,&c);
     //nn[v]=cnt;
-    nn[v]=cnt=v;//TODO remove
-    player[cnt]=0;
-    colours[cnt]=c;
-    cnt++;
+    player[v]=0;
+    colours[v]=c;
   }
   for(int i=0;i<n1;i++)
   {
     int v,c;
     scanf("%d%d",&v,&c);
-    //nn[v]=cnt;
-    nn[v]=cnt=v;//TODO remove
-    player[cnt]=1;
-    colours[cnt]=c;
-    cnt++;
+    player[v]=1;
+    colours[v]=c;
   }
   for(int i=0;i<m;i++)
   {
     int a,b;
     scanf("%d%d",&a,&b);
-    G_or[nn[a]].push_back(nn[b]);
-    G_rev[nn[b]].push_back(nn[a]);
+    G_or[a].push_back(b);
+    G_rev[b].push_back(a);
   }
   scanf("%d",&V);
-  V=nn[V];
 }
 bool implies(bool a,bool b)
 {
@@ -89,12 +81,12 @@ subgraph attr(const subgraph &G, const subgraph &U, int cur_player)
     for(int e:G)
     {
       if(res.count(e))continue;
-      if(player[e]==cur_player and std::any_of(G_or[e].begin(),G_or[e].end(),[U,G](int x){return G.count(x) && U.count(x);}))
+      if(player[e]==cur_player and std::any_of(G_or[e].begin(),G_or[e].end(),[U,G](int x){return G.count(x)==1 && U.count(x)==1;}))
       {
         res.insert(e);
         cont=true;
       }
-      if(player[e]!=cur_player and std::all_of(G_or[e].begin(),G_or[e].end(),[U,G](int x){return implies(G.count(x),U.count(x));}))
+      if(player[e]!=cur_player and std::all_of(G_or[e].begin(),G_or[e].end(),[U,G](int x){return implies(G.count(x)==1,U.count(x)==1);}))
       {
         res.insert(e);
         cont=true;
@@ -103,7 +95,8 @@ subgraph attr(const subgraph &G, const subgraph &U, int cur_player)
   }
   return res;
 }
-std::tuple<vertices,vertices> solve(const subgraph& G)//returns winning set for player 0 on graph G
+std::tuple<vertices,vertices> solve(const subgraph& G)
+//returns winning set for player 0,winning set for player 2 on graph G
 //https://arxiv.org/pdf/1307.4465.pdf
 {
   if(G.size()==0)return make_tuple(vertices(),vertices());
@@ -114,7 +107,7 @@ std::tuple<vertices,vertices> solve(const subgraph& G)//returns winning set for 
   {
     max_prio=std::max(max_prio,colours[e]);
   }
-  unsigned int p=max_prio&1;
+  unsigned int p=max_prio %2;
   vertices U;
   for(int e:G)
     if(colours[e]==max_prio)
@@ -152,7 +145,7 @@ int main()
   p0=attr(full,v,0);
   */
 
-  printf("%d\n",p0.size());
+  printf("%lu\n",p0.size());
   for(int e:p0)
     printf("%d ",e);
   printf("\n");
